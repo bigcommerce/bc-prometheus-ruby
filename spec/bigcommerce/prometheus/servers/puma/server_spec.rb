e362147ac6748c5bdc5e97e7a0d53126696977a9
Willem Homan <willlem.homan@commerce.com>
2026-08-08 14:18:45 +1000

    test(platform): PAYMENTS-12054 Close the puma server listeners after each example Bind the puma server specs to an ephemeral port

--- spec/bigcommerce/prometheus/servers/puma/server_spec.rb
+++ spec/bigcommerce/prometheus/servers/puma/server_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe Bigcommerce::Prometheus::Servers::Puma::Server do
-  let(:server) { described_class.new(port: default_port) }
-  let(:default_port) { 9800 + rand(100) }
+  let(:server) { @server = described_class.new(port: default_port) }
+  let(:default_port) { 0 }
   before do
     Bigcommerce::Prometheus.reset
   end
+  after { @server&.binder&.close }
 
   context 'when the server is initialized' do
     it 'has a valid rack app' do
